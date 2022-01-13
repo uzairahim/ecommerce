02766345ec145ab0ec23f666837de80f81892f80
HassanAli1998 <88315221+HassanAli1998@users.noreply.github.com>
2022-01-13 13:18:28 +0500

    Update index.blade.php

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -87,84 +87,11 @@
                         <!-- SLIDE  -->
                         <li data-index="rs-16" data-transition="zoomout" data-slotamount="default"
                             data-easein="Power4.easeInOut" data-easeout="Power4.easeInOut" data-masterspeed="2000"
-                            data-thumb="{{asset('assets/images/banner-1.jpeg')}}" data-rotate="0"
+                            data-rotate="0"
                             data-fstransition="fade" data-fsmasterspeed="1500" data-fsslotamount="7"
                             data-saveperformance="off" data-title="Intro" data-description="">
                             <!-- MAIN IMAGE -->
                             <img style="background-size: 100% 100vh; background-position: 100% 0;" class="banner-imageee" src="{{asset('assets/images/banner-1.jpeg')}}" alt="">
-                            <!-- LAYERS -->
-
-                            <!-- LAYER NR. 1 -->
-                            <div class="tp-caption NotGeneric-Title   tp-resizeme rs-parallaxlevel-0 text-uppercase"
-                                 id="slide-16-layer-1"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
-                                 data-lineheight="['70','70','70','50']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="x:[105%];z:0;rX:45deg;rY:0deg;rZ:90deg;sX:1;sY:1;skX:0;skY:0;s:2000;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1000"
-                                 data-splitin="chars"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-                                 data-elementdelay="0.05"
-
-                                 style="z-index: 5; white-space: nowrap;color:black;">Product N0: RC 1100
-                            </div>
-
-                            <!-- LAYER NR. 2 -->
-                            <div class="tp-caption NotGeneric-SubTitle   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-16-layer-4"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['52','52','52','51']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1500"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-
-                                 style="z-index: 6; white-space: nowrap;color: black;">Train Sound Air Horns for All Heavy Vehicles
-                            </div>
-
-                            <!-- LAYER NR. 3 -->
-                            <div class="tp-caption NotGeneric-Icon   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-16-layer-8"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['-68','-68','-68','-68']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-                                 data-style_hover="cursor:default;"
-
-                                 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:1500;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="2000"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-
-                                 style="z-index: 7; white-space: nowrap;"><i class="pe-7s-refresh"></i></div>
-
                         </li>
                         <!-- SLIDE  -->
                         <li data-index="rs-18" data-transition="zoomin" data-slotamount="7"
@@ -173,75 +100,6 @@
                             data-saveperformance="off" data-title="Ken Burns" data-description="">
                             <!-- MAIN IMAGE -->
                             <img src="{{asset('assets/images/banner-2.png')}}" alt="">
-                            <!-- LAYERS -->
-
-                            <!-- LAYER NR. 1 -->
-                            <div class="tp-caption tp-shape tp-shapewrapper   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-18-layer-9"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['15','15','15','15']"
-                                 data-width="600"
-                                 data-height="155"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:0px;"
-                                 data-mask_out="x:inherit;y:inherit;"
-                                 data-start="2000"
-                                 data-responsive_offset="on"
-
-
-                                 style="z-index: 5;background-color:rgba(0, 0, 0, 0.75);border-color:rgba(0, 0, 0, 0.50);"></div>
-
-                            <!-- LAYER NR. 2 -->
-                            <div class="tp-caption NotGeneric-Title   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-18-layer-1"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
-                                 data-lineheight="['70','70','70','50']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="y:[-100%];z:0;rZ:35deg;sX:1;sY:1;skX:0;skY:0;s:2000;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1000"
-                                 data-splitin="chars"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-                                 data-elementdelay="0.05"
-
-                                 style="z-index: 6; white-space: nowrap;">Mega Sale
-                            </div>
-
-                            <!-- LAYER NR. 3 -->
-                            <div class="tp-caption NotGeneric-SubTitle   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-18-layer-4"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['52','51','51','31']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1500"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-
-                                 style="z-index: 7; white-space: nowrap;">6 Trumpet Air Horn
-                            </div>
                         </li>
                         <!-- SLIDE  -->
                         <li data-index="rs-19" data-transition="zoomout" data-slotamount="default"
@@ -250,96 +108,6 @@
                             data-saveperformance="off" data-title="" data-description="">
                             <!-- MAIN IMAGE -->
                             <img src="{{asset('assets/images/banner-3.png')}}" alt="">
-                            <!-- LAYERS -->
-
-                            <!-- LAYER NR. 1 -->
-                            <div class="tp-caption tp-shape tp-shapewrapper   rs-parallaxlevel-0"
-                                 id="slide-19-layer-10"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
-                                 data-width="full"
-                                 data-height="full"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="opacity:0;s:2000;e:Power3.easeInOut;"
-                                 data-transform_out="opacity:0;s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-start="2000"
-                                 data-basealign="slide"
-                                 data-responsive_offset="on"
-                                 data-responsive="off"
-
-                                 style="z-index: 5;background-color:rgba(0, 0, 0, 0.25);border-color:rgba(0, 0, 0, 0);"></div>
-
-                            <!-- LAYER NR. 2 -->
-                            <div class="tp-caption NotGeneric-Title   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-19-layer-1"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']"
-                                 data-lineheight="['70','70','70','50']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1000"
-                                 data-splitin="chars"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-                                 data-elementdelay="0.05"
-
-                                 style="z-index: 6; white-space: nowrap;color: black;">Hot deals
-                            </div>
-
-                            <!-- LAYER NR. 3 -->
-                            <div class="tp-caption NotGeneric-SubTitle   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-19-layer-4"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['52','52','52','51']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-
-                                 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="1500"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-
-                                 style="z-index: 7; white-space: nowrap;color:black;">3 Trumpet Air Horn
-                            </div>
-
-                            <!-- LAYER NR. 4 -->
-                            <div class="tp-caption NotGeneric-Icon   tp-resizeme rs-parallaxlevel-0"
-                                 id="slide-19-layer-8"
-                                 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
-                                 data-y="['middle','middle','middle','middle']" data-voffset="['-68','-68','-68','-68']"
-                                 data-width="none"
-                                 data-height="none"
-                                 data-whitespace="nowrap"
-                                 data-transform_idle="o:1;"
-                                 data-style_hover="cursor:default;"
-
-                                 data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:1500;e:Power4.easeInOut;"
-                                 data-transform_out="y:[100%];s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;"
-                                 data-mask_in="x:0px;y:[100%];s:inherit;e:inherit;"
-                                 data-mask_out="x:inherit;y:inherit;s:inherit;e:inherit;"
-                                 data-start="2000"
-                                 data-splitin="none"
-                                 data-splitout="none"
-                                 data-responsive_offset="on"
-
-
-                                 style="z-index: 8; white-space: nowrap;"><i class="pe-7s-ball"></i></div>
                         </li>
                         <!-- SLIDE  -->
                     </ul>
