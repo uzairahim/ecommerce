fd61d4ee9aa22dcdf01ba08f54049dc2e31933d8
HassanAli1998 <88315221+HassanAli1998@users.noreply.github.com>
2022-01-18 17:23:26 +0500

    Create about-us.blade.php

---
+++ backend/resources/views/about-us.blade.php
@@ -0,0 +1,39 @@
+<section id="about-us">
+    <div class="brand-logo">
+        <div class="container">
+            <div class="new_title center">
+                <h1 class="text-uppercase">About Us</h1>
+                <div class="starSeparator"></div>
+            </div>
+            <div class="slider-items-products">
+                <div id="brand-logo-slider" class="product-flexslider hidden-buttons">
+                    <blockquote class="blockquote">
+                        <h4><b>MSK-SuperHorn</b></h4>
+                        Super Horn House established back in 1972 offers a premium line of automotive air horns,
+                        for
+                        virtually every conceivable application. Whether you are looking for a loud Train horn,
+                        Truck Horn,
+                        Marine Horn, SUV, Car or even for your bike, you have arrived at the right place.
+                        <br>
+                        At Super Horn House we understand the core of Air Horn Business, but we also bring
+                        something
+                        more to the table. IDEAS !! fresh way of solving problems that can help our client
+                        business.
+                        If your
+                        business has special needs or if you are simply searching for a better way you are
+                        welcome
+                        to speak
+                        with our manufacturing unit...<br> we have ideas to share. The science of auto horns,
+                        Air Horns,
+                        electric
+                        horns and even individual parts.
+                        Super Horn House aims to maximise its success by finding and processing of better
+                        resources
+                        – areas
+                        of expertise in which we have a clear competitive advantage
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
